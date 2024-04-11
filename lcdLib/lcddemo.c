1b17fff4f3157a2d271346c244ccdc64f1fa0dee
bvillarreal4 <bvillarreal4@miners.utep.edu>
2024-04-11 14:17:22 -0600

    changed what was drawn on the display

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -16,8 +16,26 @@ main()
 
   clearScreen(COLOR_BLUE);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  //drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
 
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  //fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+
+  short centerCol = screenWidth >> 1;
+  short centerRow = screenHeight >> 1;
+
+  int x = 0;
+  int y = 0;
+  int speed = 6;
+
+  for(short i = 0; i < screenWidth; i++){
+    drawPixel(x, centerRow - y, COLOR_PINK);
+    drawPixel(x, centerRow - speed, COLOR_GREEN);
+    x ++;
+    y += speed;
+    speed -= 1;
+    if ( y <= 0){
+      speed = -speed;
+    }
+  }
   
 }
