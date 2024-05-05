d0a7197ac7044d94fcfb245e12b52b1435bff8bf
bvillarreal4 <bvillarreal4@miners.utep.edu>
2024-05-04 23:26:19 -0600

    made first button draw a smiley face

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -1,10 +1,15 @@
-/** \file lcddemo.c
- *  \brief A simple demo that draws a string and square
- */
-
 #include <libTimer.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
+#include <msp430.h>
+
+#define SW1 BIT0
+#define SW2 BIT1
+#define SW3 BIT2
+#define SW4 BIT3
+
+#define switches (SW1 | SW2 | SW3 | SW4)
+
 
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int
@@ -15,27 +20,42 @@ main()
   u_char width = screenWidth, height = screenHeight;
 
   clearScreen(COLOR_BLUE);
-
-  //drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
-
-  //fillRectangle(30,30, 60, 60, COLOR_ORANGE);
-
-  short centerCol = screenWidth >> 1;
-  short centerRow = screenHeight >> 1;
-
-  int x = 0;
-  int y = 0;
-  int speed = 6;
-
-  for(short i = 0; i < screenWidth; i++){
-    drawPixel(x, centerRow - y, COLOR_PINK);
-    drawPixel(x, centerRow - speed, COLOR_GREEN);
-    x ++;
-    y += speed;
-    speed -= 1;
-    if ( y <= 0){
-      speed = -speed;
-    }
-  }
   
+  P2REN |= switches;
+  P2IE |= switches;
+  P2OUT |= switches;
+
+  or_sr(0x18);
+
+  
+}
+
+void __interrupt_vec(PORT2_VECTOR) Port_2(){
+
+
+
+  if(P2IFG & SW1){
+    P2IFG &= ~SW1;
+    switch_interrupt_handler_SW1();
+  }
+}
+
+
+void switch_interrupt_handler_SW1(){
+  char p2 = P2IN;
+
+  P2IES |= (p2 & switches);
+  P2IES &= (p2 | ~switches);
+
+  if(p2 & SW1){
+    clearScreen(COLOR_BLUE);
+  }
+  else{
+    fillRectangle(15,30, 100, 100, COLOR_ORANGE);
+    fillRectangle(35,50,20,20,COLOR_BLACK);
+    fillRectangle(75,50,20,20,COLOR_BLACK);
+    fillRectangle(35,100,58,20,COLOR_BLACK);
+    fillRectangle(25,85,20,20,COLOR_BLACK);
+    fillRectangle(85,85,20,20,COLOR_BLACK);
+  }
 }
