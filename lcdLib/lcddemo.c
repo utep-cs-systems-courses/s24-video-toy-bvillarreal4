3b8c92fc1ff0a4afff4d38af674f25c1105facd2
bvillarreal4 <bvillarreal4@miners.utep.edu>
2024-05-05 20:10:05 -0600

    made button 3 to draw a basketball net

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -42,6 +42,10 @@ void __interrupt_vec(PORT2_VECTOR) Port_2(){
     P2IFG &= ~SW2;
     switch_interrupt_handler_SW2();
   }
+  if(P2IFG & SW3){
+    P2IFG &= ~SW3;
+    switch_interrupt_handler_SW3();
+  }
 }
 
 
@@ -55,6 +59,7 @@ void switch_interrupt_handler_SW1(){
     // clearScreen(COLOR_BLUE);
   }
   else{
+    clearScreen(COLOR_BLUE);
     fillRectangle(15,30, 100, 100, COLOR_ORANGE);
     fillRectangle(35,50,20,20,COLOR_BLACK);
     fillRectangle(75,50,20,20,COLOR_BLACK);
@@ -93,4 +98,43 @@ void switch_interrupt_handler_SW2(){
 
   }
   
+}
+
+void switch_interrupt_handler_SW3(){
+  char p2 = P2IN;
+
+  P2IES |= (p2 & switches);
+  P2IES &= (p2 & ~switches);
+
+  if(p2 & SW3){
+  }
+  else{
+    clearScreen(COLOR_BLUE);
+
+    //basketball goal
+    fillRectangle(50,70, 30, 100, COLOR_BLACK);
+    fillRectangle(35,40,60,50,COLOR_WHITE);
+
+    for(short i = 0; i < 30;i++){
+      drawPixel(50+i,60, COLOR_RED);
+      drawPixel(50 + i, 80, COLOR_RED);
+    }
+    for(short i = 0; i < 20; i++){
+      drawPixel(50,60+i,COLOR_RED);
+      drawPixel(80,60+i,COLOR_RED);
+    }
+    fillRectangle(50,80,30,5,COLOR_ORANGE);
+
+    for(short i = 0; i <30; i++){
+      drawPixel(50+i, 80 + i, COLOR_GRAY);
+      drawPixel(50+i, 85+i, COLOR_GRAY);
+      drawPixel(80-i,80+i,COLOR_GRAY);
+      drawPixel(80-i, 85 + i, COLOR_GRAY);
+    }
+    for(short i = 0; i < 35; i++){
+      drawPixel(50,80+i,COLOR_GRAY);
+      drawPixel(80,80+i,COLOR_GRAY);
+    }
+    fillRectangle(50,115,30,1,COLOR_GRAY);
+  }
 }
